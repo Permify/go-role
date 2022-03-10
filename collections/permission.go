cf995befcd097f0ad223c0bf1b392dbaffafaddf
Tolga Özen <mtolgaozen@gmail.com>
2022-03-10 13:39:22 +0300

    added clear comments

--- collections/permission.go
+++ collections/permission.go
@@ -4,20 +4,27 @@ import (
 	`github.com/Permify/permify-gorm/models`
 )
 
-// Permission */
+// Permission
+// permission collections provides methods for you to manage array data more easily.
 type Permission []models.Permission
 
-// Origin */
+// Origin
+// convert the collection to permission array.
+// @return []models.Permission
 func (u Permission) Origin() []models.Permission {
 	return []models.Permission(u)
 }
 
-// Len */
+// Len
+// returns the number of elements of the array.
+// @return int64
 func (u Permission) Len() (length int64) {
 	return int64(len(u))
 }
 
-// IDs */
+// IDs
+// returns an array of the permission array's ids.
+// @return []uint
 func (u Permission) IDs() (IDs []uint) {
 	for _, permission := range u {
 		IDs = append(IDs, permission.ID)
@@ -26,6 +33,8 @@ func (u Permission) IDs() (IDs []uint) {
 }
 
 // Names */
+// returns an array of the permission array's names.
+// @return []string
 func (u Permission) Names() (names []string) {
 	for _, permission := range u {
 		names = append(names, permission.Name)
@@ -34,6 +43,8 @@ func (u Permission) Names() (names []string) {
 }
 
 // GuardNames */
+// returns an array of the permission array's guard names.
+// @return []string
 func (u Permission) GuardNames() (guards []string) {
 	for _, permission := range u {
 		guards = append(guards, permission.GuardName)
--- collections/role.go
+++ collections/role.go
@@ -5,10 +5,57 @@ import (
 	`github.com/Permify/permify-gorm/models`
 )
 
-// Role */
+// Role
+// role collections provides methods for you to manage array data more easily.
 type Role []models.Role
 
-// Permissions */
+// Origin
+// convert the collection to role array.
+// @return []models.Role
+func (u Role) Origin() []models.Role {
+	return []models.Role(u)
+}
+
+// Len
+// returns the number of elements of the array.
+// @return int64
+func (u Role) Len() (length int64) {
+	return int64(len(u))
+}
+
+// IDs
+// returns an array of the role array's ids.
+// @return []uint
+func (u Role) IDs() (IDs []uint) {
+	for _, role := range u {
+		IDs = append(IDs, role.ID)
+	}
+	return IDs
+}
+
+// Names
+// returns an array of the role array's names.
+// @return []string
+func (u Role) Names() (names []string) {
+	for _, role := range u {
+		names = append(names, role.Name)
+	}
+	return names
+}
+
+// GuardNames
+// returns an array of the permission array's guard names.
+// @return []string
+func (u Role) GuardNames() (guards []string) {
+	for _, role := range u {
+		guards = append(guards, role.GuardName)
+	}
+	return guards
+}
+
+// Permissions
+// returns uniquely the permissions of the roles in the role collection.
+// @return Permission
 func (u Role) Permissions() (permissions Permission) {
 	var IDs []uint
 	for _, a := range u {
@@ -22,38 +69,4 @@ func (u Role) Permissions() (permissions Permission) {
 		}
 	}
 	return
-}
-
-// Origin */
-func (u Role) Origin() []models.Role {
-	return []models.Role(u)
-}
-
-// Len */
-func (u Role) Len() (length int64) {
-	return int64(len(u))
-}
-
-// IDs */
-func (u Role) IDs() (IDs []uint) {
-	for _, role := range u {
-		IDs = append(IDs, role.ID)
-	}
-	return IDs
-}
-
-// Names */
-func (u Role) Names() (names []string) {
-	for _, role := range u {
-		names = append(names, role.Name)
-	}
-	return names
-}
-
-// GuardNames */
-func (u Role) GuardNames() (guards []string) {
-	for _, role := range u {
-		guards = append(guards, role.GuardName)
-	}
-	return guards
 }
